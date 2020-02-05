991178fb31cef6c54f786fff570a0c70304a18a4
Jan Noszczyk <panainz@gmail.com>
2020-02-05 22:36:43 +0000

    Reverted back to using slow scanning checking for winner, will be needed for player's get_value. Also started implementing player

--- Tic-Tac-Toe/player.py
+++ Tic-Tac-Toe/player.py
@@ -1,19 +1,27 @@
 import numpy as np
+from tic_tac_toe import check_board
 
 
 class Player:
     def __init__(self, n, number):
         self.n = n
         self.number = number
+        self.player_number = 2*number - 3
         self.value_function = {}
 
     def get_value(self, state):
-        if state in self.value_function:
-            return self.value_function[state]
-        else:
+        state_hash = str(state.reshape(state.shape[0]*state.shape[1]))
+        if state_hash not in self.value_function:
+            outcome = check_board(state)
+            if outcome == self.number:
+                self.value_function[state_hash] = 1
+            elif outcome == 0:
+                self.value_function[state_hash] = 0.5
+            else:
+                self.value_function[state_hash] = 0
+        return self.value_function[state_hash]
 
 
 
-
-a = np.ones((3**2,3**2))*0.5
-print(a)
+a = np.ones((4,4))*0.2
+print(a.shape[0])
--- Tic-Tac-Toe/tic_tac_toe.py
+++ Tic-Tac-Toe/tic_tac_toe.py
@@ -2,36 +2,61 @@ import numpy as np
 import logging
 
 
+def check_board(board):
+    """
+    Scan the board to check for a winner or see if its a draw.
+    Player 1 is marked as -1, player 2 marked as 1
+    """
+    n = board.shape[0]
+    for row in range(n):
+        row_sum = np.sum(board[row, :])
+        if row_sum == -n:
+            return 1
+        elif row_sum == n:
+            return 2
+
+    for col in range(n):
+        col_sum = np.sum(board[:, col])
+        if col_sum == -n:
+            return 1
+        elif col_sum == n:
+            return 2
+
+    diag_1, diag_2 = np.trace(board), np.trace(np.fliplr(board))
+    if diag_1 == -n or diag_2 == -n:
+        return 1
+    elif diag_1 == n or diag_2 == n:
+        return 2
+
+    for val in np.nditer(board):
+        if val == 0:
+            return 0
+    return -1
+
+
 class TicTacToe:
     def __init__(self, n):
         self.n = n
         self.current_move = 0
         self.last_player = None
         self.board = np.zeros((n, n))
-        self.rows, self.columns = [0] * n, [0] * n
-        self.diag, self.adiag = 0, 0
         self.possible_next_state = None
 
     def get_state(self):
+        return self.board
         # Represent state as a state vector, reshaping board into a vector
-        return str(self.board.reshape(self.n**2))
+        # return str(self.board.reshape(self.n**2))
 
     def play(self, player, i, j):
         if 0 <= i < self.n and 0 <= j < self.n:
             if player in [1, 2] and player != self.last_player and self.board[i, j] == 0:
-                print("Possible states are {} \n".format(self.draw_board(player)))
+                # print("Possible states are {} \n".format(self.draw_board(player)))
                 self.last_player = player
-                self.board[i, j] = player
                 self.current_move += 1
 
                 # For player 1 num is -1, for player 2 its 1
                 player_num = 2*player - 3
-                self.rows[i] += player_num
-                self.columns[j] += player_num
-                if i == j:
-                    self.diag += player_num
-                elif j == self.n - i - 1:
-                    self.adiag += player_num
+                self.board[i, j] = player_num
 
                 print("Round number {} \n {} \n{} \n ".format(self.current_move, self.board, self.game_over()))
             else:
@@ -40,22 +65,9 @@ class TicTacToe:
             logging.error("The location ({}, {}) is outside of the board".format(i, j))
 
     def game_over(self):
-        """
-        Scan the board to check for a winner or see if its a draw.
-        Player 1 is marked as -1, player 2 marked as 1
-        """
-        if -self.n in [self.diag, self.adiag] + self.rows + self.columns:
-            return 1
-        elif self.n in [self.diag, self.adiag] + self.rows + self.columns:
-            return 2
+        return check_board(self.board)
 
-        if self.current_move == self.n ** 2:
-            # draw
-            return -1
-        else:
-            return 0
-
-    def draw_board(self, player):
+    def get_possible_states(self, player):
         """
         Get a list of possible_next_states, for each empty field theres a corresponding action and new state
         :return:
@@ -68,15 +80,19 @@ class TicTacToe:
                 possible_states.append(((x, y), str(new_board.reshape(self.n**2))))
         return possible_states
 
+    def draw_board(self):
+        pass
 
 
-game = TicTacToe(3)
-game.play(1, 0, 2)
-game.play(2, 1, 0)
-game.play(1, 2, 0)
-game.play(2, 1, 1)
-game.play(1, 0, 1)
-game.play(2, 2, 2)
-game.play(1, 1, 2)
-game.play(2, 0, 0)
-# game.play(1, 2, 1)
+def test_tic_tac():
+    game = TicTacToe(3)
+    game.play(1, 0, 2)
+    game.play(2, 1, 0)
+    game.play(1, 2, 0)
+    game.play(2, 1, 1)
+    game.play(1, 0, 1)
+    game.play(2, 2, 2)
+    game.play(1, 1, 2)
+    game.play(2, 0, 0)
+    assert game.game_over() == 2
+test_tic_tac()
